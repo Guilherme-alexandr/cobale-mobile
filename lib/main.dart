70c65a04e5fdc4c7abe95ca6358f690f2a87ef1f
Guilherme-alexandr <177791893+Guilherme-alexandr@users.noreply.github.com>
2026-02-06 20:20:04 -0300

    chore: estrutura inicial do app Flutter

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'features/auth/pages/login_page.dart';
+import 'features/home/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'CobAle',
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/home': (context) => const HomePage(),
+      },
+    );
+  }
+}
